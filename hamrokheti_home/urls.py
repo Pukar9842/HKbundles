edcbda141f10b1eb625a6bc13421e1e7f73152f7
Pukar9842 <pukarniroula71@gmail.com>
2020-10-08 23:04:13 +0600

    templates

--- hamrokheti_home/urls.py
+++ hamrokheti_home/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('availablefarms', views.availablefarms)
+    path('availablefarms', views.availablefarms),
+    path('services', views.services),
+    path('about', views.about),
+    path('contact', views.contact),
+    path('buyproducts', views.buyproducts)
+
 ]
--- hamrokheti_home/views.py
+++ hamrokheti_home/views.py
@@ -5,10 +5,25 @@ from .models import FundFarm
 
 # /FundFarm-> index
 def index(request):
-    farm_items = FundFarm.objects.all()
-    return render(request, 'index.html', {'farms': farm_items})
+    return render(request, 'index.html')
 
 
 def availablefarms(request):
-    return HttpResponse('Available Farms')
+    farm_items = FundFarm.objects.all()
+    return render(request, 'availablefarms.html', {'farms': farm_items})
 
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def buyproducts(request):
+    return render(request, 'buyproducts.html')
